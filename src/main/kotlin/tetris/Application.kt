87703f3350f2f08598200b67ca6f29bdcfa62ed2
Kiselev Danil <dabikis2855@gmail.com>
2023-12-23 10:28:29 +0600

    [+] add input read and quit on 'q' input

--- src/main/kotlin/tetris/Application.kt
+++ src/main/kotlin/tetris/Application.kt
@@ -4,6 +4,7 @@ import com.googlecode.lanterna.TextColor
 import com.googlecode.lanterna.terminal.Terminal
 import common.Coord
 import di.commonDIModule
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -13,13 +14,15 @@ import tetris.component.PositionProperty
 import tetris.component.DrawProperty
 import tetris.component.MoveProperty
 import tetris.systems.DefaultDrawSystem
-import tetris.systems.GameSystem
 import tetris.systems.MoveSystem
 import tetris.systems.command.CommandQueue
+import kotlin.concurrent.thread
 
 
 open class Application {
     companion object {
+        var isRunning = true
+
         @JvmStatic
         fun main(args: Array<String>) = runBlocking {
             startKoin {
@@ -30,11 +33,43 @@ open class Application {
             println("Program arguments: ${args.joinToString()}")
             val terminal: Terminal = GlobalContext.get().get()
             println(terminal.terminalSize)
+            prepareObject()
+            val queue: CommandQueue = GlobalContext.get().get()
+            terminal.clearScreen()
 
+            runInputDaemon()
+            launch { runSys() }
+            val gameCycleResult = async {
+                while (isRunning) {
+                    delay(1000L)
+                    queue.execute()
+                }
+                println("That's end folks")
+                terminal.clearScreen()
+                terminal.close()
+
+            }
+            gameCycleResult.await()
+            return@runBlocking
+        }
+
+        private fun runInputDaemon() {
+            thread(start = true, isDaemon = true, name = "inputDaemon", block = {
+                var ch: Char = ' '
+                while (isRunning) {
+                    ch = System.`in`.read().toChar()
+                    println(ch)
+                    if (ch == 'q' || ch == 'Q') {
+                        isRunning = false
+                        println("Exit input case")
+                    }
+                }
+            })
+        }
+
+        private fun prepareObject(): GameObject {
             val circlePattern: Set<Pair<Int, Int>> =
                 setOf(Pair(0, 0), Pair(1, 0), Pair(2, 0), Pair(2, 1), Pair(2, 2), Pair(1, 2), Pair(0, 2), Pair(0, 1))
-
-
             val gameObject = GameObject(null)
 
             var position = PositionProperty(gameObject, Coord(15, 10))
@@ -47,18 +82,7 @@ open class Application {
             gameObject.addProperty("drawable", drawProperty)
             gameObject.addProperty("position", position)
             gameObject.addProperty("move", moveProperty)
-//            gameObject.props["drawable"]?.let { drawSystem.addComponent(it) }
-            val queue : CommandQueue = GlobalContext.get().get()
-            terminal.clearScreen()
-            var isRunning = true
-            launch { runSys() }
-            launch {
-                while (isRunning) {
-                    delay(1000L)
-                    queue.execute()
-                }
-            }
-            println("That's end folks")
+            return gameObject
         }
 
         private suspend fun runSys() {

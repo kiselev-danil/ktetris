cd9e83151a48554e686d1702307d72fadf7b1bda
Kiselev Danil <dabikis2855@gmail.com>
2023-12-23 08:43:44 +0600

    [~] make coord as variable inside direction enum

--- src/main/kotlin/tetris/MovementDirection.kt
+++ src/main/kotlin/tetris/MovementDirection.kt
@@ -2,6 +2,6 @@ package tetris
 
 import common.Coord
 
-enum class MovementDirection(coord: Coord) {
+enum class MovementDirection(val coord: Coord) {
     Up(Coord(0,-1)), Down(Coord(0,1)), Left(Coord(-1,0)), Right(Coord(1,0))
 }

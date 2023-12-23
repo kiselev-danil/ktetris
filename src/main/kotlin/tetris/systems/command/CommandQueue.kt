bbdbaddd0d144fddff1fffadbb5269738004c702
Kiselev Danil <dabikis2855@gmail.com>
2023-12-23 07:18:26 +0600

    [+] Queue

---
+++ src/main/kotlin/tetris/systems/command/CommandQueue.kt
@@ -0,0 +1,27 @@
+package tetris.systems.command
+
+class CommandQueue: ICommand {
+    private var queue: MutableList<ICommand> = ArrayList<ICommand>()
+    private var executed: MutableList<ICommand> = ArrayList<ICommand>()
+
+    fun push(cmd: ICommand) {
+        queue.add(queue.lastIndex + 1, cmd)
+    }
+
+    override suspend fun execute() {
+        if (queue.size > 0) {
+            queue.first().execute()
+            executed.add(executed.lastIndex + 1, queue.first())
+            queue.removeAt(0)
+        }
+    }
+
+    override suspend fun undo() {
+        if (executed.size > 0) {
+            executed.last().undo()
+            queue.add(0,executed.last())
+            executed.removeAt(executed.lastIndex)
+        }
+    }
+
+}
